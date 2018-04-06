bc621408cf91dafc9f831469a2ad1de56615e534
Haoran Wang <vitowang1998@gmail.com>
2018-04-05 23:45:50 -0400

    Adds a file about bash commands.

---
+++ bash/commands.cpp
@@ -0,0 +1,11 @@
+// code for bash
+
+// pwd - (print working directory) give current directory
+// cd - (change directory)
+// ls - list contents
+// ls -s : list contents with permissions
+// mkdir dirname - make a new directory
+// rmdir dirname - remove an empty directory
+// cp file1 file2 - make a copy of file1 and name it file2
+// rm file - remove file
+// cat a - display the contents in a
