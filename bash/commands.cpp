e847eaf200cb9822bc00c7a74baf9625500dbe09
Haoran Wang <vitowang1998@gmail.com>
2018-04-10 23:53:48 -0400

    Reformats the bash commands and adds some new commands.

--- bash/commands.cpp
+++ bash/commands.cpp
@@ -1,20 +1,48 @@
 // code for bash
 
-// pwd - (print working directory) give current directory
+// pwd - (print working directory)
+// This command yields current directory
+
+
 // cd - (change directory)
+// This command changes directory
+
+
+// cd[space]
+// This command changes directory up by one level
+
+
+// cd[space]/
+// This command changes directory to the top level
+
+
 // ls - list contents
-// ls -s : list contents with permissions
-// mkdir dirname - make a new directory
+// This command lists the contents in current directory
+
+
+// ls -l
+// This command lists contents with permissions
+
+
+// mkdir [folder_name]
+// This command makes a new directory and name it [folder_name]
+
+
 // rmdir dirname - remove an empty directory
+
+
 // cp file1 file2 - make a copy of file1 and name it file2
 // rm file - remove file
 // cat a - display the contents in a
 
-// gcc filename.c - complile a .c file with gcc compiler and name the output "a.out"
+
+// gcc filename.c
+// This command compliles a .c file with gcc compiler and name the output "a.out"
+
 
 // gcc a.c -o b
 // compile a.c with gcc and name the output b
-// b can have extension
+// b can have any extension
 
 
 // direct output to a file
