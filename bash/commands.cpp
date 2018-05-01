1ff2124e7e08bd6f61608ee08e70d1b79e681448
Haoran Wang <vitowang1998@gmail.com>
2018-05-01 11:59:04 -0400

    Adds more bash commands.

--- bash/commands.cpp
+++ bash/commands.cpp
@@ -1,8 +1,39 @@
 // code for bash
 
+/* Special Directories
+ 
+ .            current directory
+ ..           current directory parent
+ ~            home directory
+ ~userid      home directory
+ */
+
+
+/* Paths
+    Absolute Path: Starting from the root
+    Relative Path: Not starting from the root
+ */
+
+
+
+
+// echo $0
+// check which Unix Shell we are using now
+// print [bash] if we are using bash
+//       [-bash] if we aren't using bash
+
 
 // echo [string]
 // This command will display the contents of [string]
+
+
+// echo CS[!1]*
+
+
+// echo [abc]*.txt
+
+
+// echo cs{246, 1}
 
 
 // lpr [file]
@@ -37,20 +68,48 @@
 // This command lists the contents in current directory
 
 
+// ls -a
+// Lists all files (include hidden files)
+// (Hidden files' name starts with a dot(.)
+
+
 // ls -l
 // This command lists contents with permissions
+
+
+// ls *.txt
+// Wildcard matching
+// Lists all files of the form [whatever].txt
 
 
 // mkdir [folder_name]
 // This command makes a new directory and name it [folder_name]
 
 
-// rmdir dirname - remove an empty directory
+// rmdir dirname
+// Removes an empty directory. An error occurs if attempt to remove a non-empty folder.
+// This is safer.
+
+
+// rmdir -r [folder_name]
+// Removes the contents [folder_name] and removes [folder_name]
+// This can be dangerous since we're able to delete multiple files at once.
 
 
 // cp file1 file2 - make a copy of file1 and name it file2
 // rm file - remove file
 // cat a - display the contents in a
+
+
+// rm [file]
+// This will delete [file]
+// This will not permanently remove the file, it is ACTUALLY REMOVED when we reuse the memory
+// So it is technically possible to restore the files with special methods.
+
+
+// rm *.txt
+// Remove all files of the form [whatever].txt
+// It is clearly extremely dangerous.
 
 
 // gcc filename.c
