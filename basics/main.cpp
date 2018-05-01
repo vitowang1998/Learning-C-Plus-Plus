fa8f60589a0fa98f7453deeb85760f886ea8ab4e
Haoran Wang <vitowang1998@gmail.com>
2018-05-01 11:58:14 -0400

    Adds a file about main function in C++.

---
+++ basics/main.cpp
@@ -0,0 +1,10 @@
+// Every C++ function has a main function, it is the entry point of every program.
+#include <iostream>
+
+int main(void) {
+    std::cout << "Hello, World!\n";
+    
+    // "main" returns a number to OS
+    // The number is 0 if the program finishes as expected, is any other number if error occurs.
+    return 0;
+}
