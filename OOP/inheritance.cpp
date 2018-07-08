6ba062d22887301b7a36e04d2acbc3f8fd9ac8cf
Haoran Wang <vitowang1998@gmail.com>
2018-07-07 22:22:03 -0400

    Adds a file about inheritance in C++.

---
+++ OOP/inheritance.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+class SuperClass {
+    // member variables
+    int instanceVar1;
+    int instanceVar2;
+    
+public:
+    // constructor
+    SuperClass(int a, int b): instanceVar1{a}, instanceVar2{b} {}
+};
+
+
+// Inheritance: Every instance of SubClass has the member variables and methods from SuperClass
+class SubClass: public SuperClass {
+    // member variables
+    int instanceVar3;
+    
+public:
+    // constructor
+    // use the constructor of SuperClass to assign the value of instanceVar1 and instanceVar2
+    SubClass(int a, int b, int c): SuperClass{a, b}, instanceVar3{c} {}
+};
+
+
+int main() {
+
+}
