5abf9d5afca7d9833a609adbba5364acd00b6ff1
Haoran Wang <vitowang1998@gmail.com>
2018-07-19 15:48:11 -0400

    Updates the basics of vector in C++.

--- vectors/vector.cpp
+++ vectors/vector.cpp
@@ -1,6 +1,4 @@
-//  Created by Haoran Wang on 2018-01-11.
-//  Copyright © 2018 Haoran Wang. All rights reserved.
-//
+// Vector
 
 #include <iostream>
 #include <vector>
@@ -8,28 +6,41 @@
 using namespace std;
 
 
-int main(int argc, const char * argv[]) {
+// The essence of vectors are containers. Vectors contain objects.
+// Vectors can be considered as dynamic arrays.
 
-  
+
+// Vector is not a type, vector is a template class.
+// The compiler generates (instantiates) the needed class by the information we passed
+
+
+int main(int argc, const char * argv[]) {
     
-    // push_back(i) -> put i to the top of the given vector
-    // pop_back() -> returns the top element of the vector while removing it from the vector
     
+    
+    // push_back(i) -> put i to the end of the given vector
+    // pop_back() -> returns the end element of the vector while removing it from the vector
+    
+    // vector is a template class
+    // compiler instantiates a int vector class
+    // v stores objects of int
     vector<int> v;
     
     
     
+    // Adding values to a vector<int>
     for (int i = 0; i < 17; i++)
     {
         cout << "size = " << v.size() << ", " << "capacity = " << v.capacity() << endl;
-        v[i] = i;
-        v.push_back(i + 900);        cout << "currently adding value = " << v[i] << endl;
+        
+        v.push_back(i);        cout << "currently adding value = " << v[i] << endl;
         cout << "size = " << v.size() << ", " << "capacity = " << v.capacity() << endl << endl;
         
     }
     
     
-
+    
+    // Removing values from the vector
     while(v.size() != 0)
     {
         int i = v.back();
@@ -38,27 +49,13 @@ int main(int argc, const char * argv[]) {
     }
     
     
-
-    
-    
-    string month_name[12];
-    month_name[0] = "January";
-    month_name[1] = "February";
-    month_name[11] = "youmeiyoua";
-    
-    cout << month_name[0] << " " << month_name[12] << endl;
-    
     
     // this proves that the vector array can be arrenged by the commends of non-dynamic array (if it is already initilized by push_back(something);
     v[5] = 5;
     cout << v[5] << endl;
     
-
+    
     
     return 0;
 }
-
-
-
-
 
